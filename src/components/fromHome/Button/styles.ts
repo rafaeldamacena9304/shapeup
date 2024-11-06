7c87c4029faaf47e44f2cfd22be367311b01791f
rafael <rafaeldamacena9304@gmail.com>
2024-11-06 20:17:04 -0300

    Fix: wrong directory HomePage

--- src/components/fromHome/Button/styles.ts
+++ src/components/fromHome/Button/styles.ts
@@ -3,10 +3,18 @@ import styled from "styled-components";
 export const Button = styled.button`
   padding: 16px 48px;
   background-image: linear-gradient(to right, var(--darkBlue), var(--lightBlue));
-  font-size: 18px;
-  font-weight: bold;
+  font-size: 20px;
+  font-weight:bold;
   margin-bottom: 24px;
   border:none;
   color:white;
   border-radius:32px;
+  transition: all .5s;
+
+  &:hover{
+    cursor:pointer;
+    transform:scale(1.05);
+    transition: all .5s;
+  }
+
 `;
