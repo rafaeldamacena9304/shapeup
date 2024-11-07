9295af0263dbc12822a9fa4ebae78ae3deeb4608
rafael <rafaeldamacena9304@gmail.com>
2024-11-07 00:09:52 -0300

    fix: removed testCard from homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,21 +4,10 @@ import { Container } from "../../components/fromHome/Container";
 import { Logo } from "../../components/fromHome/Logo";
 import { Text } from "../../components/fromHome/Text";
 import { Wrapper } from "../../components/fromHome/Wrapper";
-import { MainCard } from "../../components/fromDashboard/MainCard";
-import dumbbell from "../../assets/images/dumbbell.png";
-
-const conteudos = ["Pulley Frente", "Remada", "Agachamento"];
 
 export const HomePage = () => {
   return (
     <>
-      <MainCard
-        content={conteudos}
-        image={dumbbell}
-        title="Treino de hoje"
-        subType="Costas"
-        status="Pendente"
-      />
       <Container>
         <>
           <Wrapper forImage={false}>
